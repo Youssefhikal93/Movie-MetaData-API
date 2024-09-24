b35aaf361cc16611a7dbf0c6bed43bff7a305751
Youssef Hikal <140002522+Youssefhikal93@users.noreply.github.com>
2024-09-24 19:59:10 +0200

    Update swaggerConfig.ts

--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -11,8 +11,8 @@ const options = {
     },
     servers: [
       {
-        // url: 'http://localhost:3000',
-        url: 'https://movie-metadata-api.onrender.com',
+        url: 'http://localhost:3000',
+        // url: 'https://movie-metadata-api.onrender.com',
       },
     ],
     components: {
